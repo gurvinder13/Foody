e8f4dc0ac22aa0566e51d80c83b135435c347a64
Gurvinder Singh <gurvinderscs@gmail.com>
2020-12-13 08:08:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/foodydemo/util/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        //const val API_KEY = "[YOUR API KEY HERE]"
+        const val API_KEY = "d16fba4a3aea4b79afbcd5a35d40bd1b"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+    }
+
+}

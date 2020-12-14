8caa6494dcf0815c04b3ddb7bbaa54505049429f
Gurvinder Singh <gurvinderscs@gmail.com>
2020-12-14 14:51:17 +0530

    database created

---
+++ app/src/main/java/com/example/foodydemo/data/database/RecipesDao.kt
@@ -0,0 +1,16 @@
+package com.example.foodydemo.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM  recipes_table ORDER By id ASC ")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+}
